1dd6930c0ae0ed090f654f16baa90b8bc50c95fb
kardaszb <Bartek21>
2020-06-02 22:45:39 +0200

    adding updates

--- src/Utils.java
+++ src/Utils.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class Utils {
 
     static String readSeasonFromUser() {
-        String valueFromUser;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Podaj pore roku ktorej miesiace chcesz wyświetlic ( ");
         for (Season season : Season.values()) {
             System.out.print(season.name() + " ");
         }
         System.out.println("):");
-        return valueFromUser = scanner.nextLine();
+        return scanner.nextLine();
     }
 
     static String getSeasonInfo(String valueFromUser) {
-        String seasonInfo;
-        return seasonInfo = Season.valueOf(valueFromUser).toString();
+        return Season.valueOf(valueFromUser).toString();
     }
 
 
